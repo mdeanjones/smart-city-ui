843b47241fb4354f7d452222f0192644ee0c5f98
Michael Jones <mdeanjones@gmail.com>
2017-03-14 12:20:41 -0400

    Moved load into service.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,25 +1,14 @@
 import Ember from 'ember';
 
-const { Route, inject, get, RSVP, setProperties } = Ember;
+const { Route, inject, get } = Ember;
 
 
 export default Route.extend({
   mapService: inject.service('smart-city-maps'),
 
-  ajax: inject.service(),
-
 
   // Load fixture data when the application first initializes.
   beforeModel() {
-    const ajax = get(this, 'ajax');
-    const store = get(this, 'mapService.store');
-
-    return RSVP.hash({
-      evLocations: ajax.request('/ev_station_locations'),
-      chargingStations: ajax.request('/existing_charging_stations'),
-      gasLocations: ajax.request('/gas_station_locations')
-    }).then((results) => {
-      setProperties(store, results);
-    });
+    return get(this, 'mapService.store').load();
   },
 });
--- app/services/smart-city-dataset.js
+++ app/services/smart-city-dataset.js
@@ -1,12 +1,27 @@
 import Ember from 'ember';
 
-const { Service } = Ember;
+const { Service, inject, RSVP, get, setProperties } = Ember;
 
 
 export default Service.extend({
+  ajax: inject.service(),
+
   gasLocations: null,
 
   evLocations: null,
 
   chargingStations: null,
+
+
+  load() {
+    const ajax = get(this, 'ajax');
+
+    return RSVP.hash({
+      evLocations: ajax.request('/ev_station_locations'),
+      chargingStations: ajax.request('/existing_charging_stations'),
+      gasLocations: ajax.request('/gas_station_locations')
+    }).then((results) => {
+      setProperties(this, results);
+    });
+  },
 });
