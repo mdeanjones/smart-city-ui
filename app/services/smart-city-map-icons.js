6fa0a05612d3c14def8e3ae2d907faae95321c42
Michael Jones <mdeanjones@gmail.com>
2017-03-14 12:20:51 -0400

    Added images

--- app/services/smart-city-map-icons.js
+++ app/services/smart-city-map-icons.js
@@ -7,26 +7,26 @@ const { Service, computed } = Ember;
 export default Service.extend({
   gasLocationIcon: computed(function() {
     return L.icon({
-      iconUrl: '/assets/images/gas-station-map-icon.png',
-      shadowUrl: '/assets/images/gas-station-map-icon.png',
+      iconUrl: '/assets/images/icon_gas.png',
+      // shadowUrl: '/assets/images/icon_gas.png',
       iconSize: [30, 30],     // size of the icon
-      shadowSize: [50, 64],   // size of the shadow
-      iconAnchor: [22, 94],   // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62],  // the same for the shadow
-      popupAnchor: [-3, -76]  // point from which the popup should open relative to the iconAnchor
+      // shadowSize: [50, 64],   // size of the shadow
+      // iconAnchor: [22, 94],   // point of the icon which will correspond to marker's location
+      // shadowAnchor: [4, 62],  // the same for the shadow
+      // popupAnchor: [-3, -76]  // point from which the popup should open relative to the iconAnchor
     });
   }).readOnly(),
 
 
   evLocationIcon: computed(function () {
     return L.icon({
-      iconUrl: '/assets/images/ev-station-map-icon.png',
-      shadowUrl: '/assets/images/ev-station-map-icon.png',
+      iconUrl: '/assets/images/icon_fastcharge.png',
+      // shadowUrl: '/assets/images/icon_fastcharge.png',
       iconSize: [35, 35],     // size of the icon
-      shadowSize: [50, 64],   // size of the shadow
-      iconAnchor: [22, 94],   // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62],  // the same for the shadow
-      popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+      // shadowSize: [50, 64],   // size of the shadow
+      // iconAnchor: [22, 94],   // point of the icon which will correspond to marker's location
+      // shadowAnchor: [4, 62],  // the same for the shadow
+      // popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
     });
   }).readOnly(),
 });
