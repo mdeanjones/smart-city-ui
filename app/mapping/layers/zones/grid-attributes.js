6ac439d4154024a9140f1625bfb66679b185be61
Michael Jones <mdeanjones@gmail.com>
2017-03-17 15:34:09 -0400

    Attribute rollup computed and added to map tooltips.

--- app/mapping/layers/zones/grid-attributes.js
+++ app/mapping/layers/zones/grid-attributes.js
@@ -3,7 +3,9 @@ import Zone from './-zone';
 import Ember from 'ember';
 
 
-const { get } = Ember;
+const {
+  get,
+} = Ember;
 
 
 export default Zone.extend({
@@ -22,16 +24,79 @@ export default Zone.extend({
   createRectangle(bounds, polygonProperties, record) {
     const rectangle = this._super(bounds, polygonProperties);
     const popup = L.popup({ maxWidth: 170 });
+    const zones = [];
+
+
+    console.log(get(record, 'agricultureRollupValue'));
+
+
+    if (get(record, 'agricultureRollupValue')) {
+      zones.push('Agriculture');
+    }
+
+    if (get(record, 'commercialRollupValue')) {
+      zones.push('Commercial');
+    }
+
+    if (get(record, 'downtownRollupValue')) {
+      zones.push('Downtown');
+    }
+
+    if (get(record, 'industrialRollupValue')) {
+      zones.push('Industrial');
+    }
+
+    if (get(record, 'parkingRollupValue')) {
+      zones.push('Parking');
+    }
+
+    if (get(record, 'publicLandRollupValue')) {
+      zones.push('Public Land');
+    }
+
+    if (get(record, 'residentialSingleRollupValue')) {
+      zones.push('Residential Single Family');
+    }
+
+    if (get(record, 'residentialMultiRollupValue')) {
+      zones.push('Residential Multi-family');
+    }
+
 
     const content = `
-        <span class="text-muted">Features:</span>
-        <br />
-        <ul class="list-unstyled">
-            <li><strong>${get(record, 'gasStations')}</strong> Gas Stations</li>
-            <li><strong>${get(record, 'busStops')}</strong> Bus Stops</li>
-            <li><strong>${get(record, 'schools')}</strong> Schools</li>
-            <li><strong>${get(record, 'isPark')}</strong> Parks</li>
-        </ul>
+        <p class="features-heading">Features:</p>
+        
+        <table class="features-table">
+            <tbody>
+                <tr>
+                    <td><strong></strong></td>
+                    <td>Proposed Charging Stations</td>
+                </tr>
+                <tr>
+                    <td><strong></strong></td>
+                    <td>Existing Charging Stations</td>
+                </tr>
+                <tr>
+                    <td><strong>${get(record, 'gasStations')}</strong></td>
+                    <td>Gas Stations</td>
+                </tr>
+                <tr>
+                    <td><strong>${get(record, 'busStops')}</strong></td>
+                    <td>Bus Stops</td>
+                </tr>
+                <tr>
+                    <td><strong>${get(record, 'schools')}</strong></td>
+                    <td>Schools</td>
+                </tr>
+                <tr>
+                    <td><strong>${get(record, 'isPark')}</strong></td>
+                    <td>Parks</td>
+                </tr>
+            </tbody>
+        </table>
+        
+        <p class="zones-heading">Zones:</p>
+        <p class="zones-list">${zones.join(', ')}</p>
     `;
 
     popup.setContent(content);
