a47a7e64c7dc1f5c10232f54141d58e5f0e8198a
Michael Jones <mdeanjones@gmail.com>
2017-03-20 15:46:26 -0400

    Moved popup creation to its own method. Might be useful later.

--- app/mapping/layers/rectangles/grid-attributes.js
+++ app/mapping/layers/rectangles/grid-attributes.js
@@ -39,6 +39,13 @@ export default Zone.extend({
 
   createRectangle(bounds, polygonProperties, record) {
     const rectangle = this._super(bounds, polygonProperties);
+
+    rectangle.bindPopup(this.createRectanglePopup(record));
+    return rectangle;
+  },
+
+
+  createRectanglePopup(record) {
     const popup = L.popup({ maxWidth: 170 });
     const zones = [];
 
@@ -115,8 +122,7 @@ export default Zone.extend({
     `;
 
     popup.setContent(content);
-    rectangle.bindPopup(popup);
 
-    return rectangle;
+    return popup;
   },
 });
