3fd704f39437f3288abcd81d6cd74a69c509b69b
Michael Jones <mdeanjones@gmail.com>
2017-03-21 08:48:31 -0400

    Toggle grid score color gradient.

--- app/mapping/layers/rectangles/grid-attributes.js
+++ app/mapping/layers/rectangles/grid-attributes.js
@@ -1,5 +1,5 @@
 /* global L */
-import Zone from './-rectangle';
+import Rectangle from './-rectangle';
 import Ember from 'ember';
 
 
@@ -9,7 +9,7 @@ const {
 } = Ember;
 
 
-export default Zone.extend({
+export default Rectangle.extend({
   showScoreColors: false,
 
   isVisible: true,
@@ -24,14 +24,60 @@ export default Zone.extend({
   },
 
 
+  scoreColorPolygonProperties: {
+    score1: {
+      fillColor: '#ce0e00',
+      fillOpacity: 0.5,
+    },
+
+    score2: {
+      fillColor: '#ca4b00',
+      fillOpacity: 0.5,
+    },
+
+    score3: {
+      fillColor: '#c68500',
+      fillOpacity: 0.5,
+    },
+
+    score4: {
+      fillColor: '#c2bd00',
+      fillOpacity: 0.5,
+    },
+
+    score5: {
+      fillColor: '#8bbf00',
+      fillOpacity: 0.5,
+    },
+
+    score6: {
+      fillColor: '#19c600',
+      fillOpacity: 0.5,
+    },
+  },
+
+
   showScoreColorObserver: observer('showScoreColors', function() {
     const showScoreColors = get(this, 'showScoreColors');
+    const layer = get(this, 'layer');
 
     if (showScoreColors) {
+      const styles = get(this, 'scoreColorPolygonProperties');
 
+      layer.getLayers().forEach((layer) => {
+        const score = get(layer, 'smartCityData.score');
+
+        if (score > 0 && score < 7) {
+          layer.setStyle(get(styles, `score${score}`));
+        }
+      });
     }
     else {
+      const defaultStyles = get(this, 'polygonProperties');
 
+      layer.getLayers().forEach((layer) => {
+        layer.setStyle(defaultStyles);
+      });
     }
   }),
 
@@ -54,6 +100,11 @@ export default Zone.extend({
 
   createRectangle(bounds, polygonProperties, record) {
     const rectangle = this._super(bounds, polygonProperties);
+
+    rectangle.smartCityData = {
+      recordId: get(record, 'id'),
+      score: get(record, 'score'),
+    };
 
     rectangle.bindPopup(this.createRectanglePopup(record));
     return rectangle;
