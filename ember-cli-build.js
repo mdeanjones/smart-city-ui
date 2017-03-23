8fa6062ae73b787b4b6958411ca154379546e35f
Michael Jones <mdeanjones@gmail.com>
2017-03-23 08:05:33 -0400

    More styles refactoring, and tooltips.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -28,6 +28,10 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('bower_components/tooltipster/dist/css/tooltipster.main.css');
+  app.import('bower_components/tooltipster/dist/css/plugins/tooltipster/sideTip/themes/tooltipster-sideTip-light.min.css');
+  app.import('bower_components/tooltipster/dist/js/tooltipster.bundle.js');
+
   app.import('bower_components/leaflet/dist/leaflet.css');
   app.import('bower_components/leaflet/dist/leaflet.js');
 
