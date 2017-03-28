b68110cbba6eb2efaecf194e3130ba8e403a3bdb
Michael Jones <mdeanjones@gmail.com>
2017-03-28 12:46:12 -0400

    School zone rectanges

---
+++ app/mapping/layers/rectangles/school.js
@@ -0,0 +1,12 @@
+import Zone from './-rectangle';
+
+
+export default Zone.extend({
+  polygonProperties: {
+    color: '#000099',
+    fillColor: '#3366FF',
+    fillOpacity: 0.4,
+    weight: 1,
+    dashArray: [5, 5],
+  },
+});
